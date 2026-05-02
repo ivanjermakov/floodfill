19d14480b42d06c939a0b4e1bba2dff437ec8b94
ivanjermakov <ivanjermakov1@gmail.com>
2026-05-02 11:05:41 +0200

    Build highway db

--- src/preprocess.ts
+++ src/preprocess.ts
@@ -1,70 +1,133 @@
 import { createReadStream } from 'fs'
-import { writeFile } from 'fs/promises'
 import sax from 'sax'
+import { open } from 'sqlite'
+import sqlite3 from 'sqlite3'
 
-const pipeline = async (opentag: (e: sax.Tag) => void) => {
-    const fileStream = createReadStream('resource/planet_20.423,51.941_21.793,52.53.osm')
+const sql = String.raw
+
+const pipeline = async (opentag: (ctx: { parent?: sax.Tag; nds: string[] }, e: sax.Tag) => void) => {
+    // const fileStream = createReadStream('resource/planet_20.423,51.941_21.793,52.53.osm')
+    const fileStream = createReadStream('resource/planet_20.967,52.167_21.071,52.212.osm')
     // const fileStream = createReadStream('resource/small.osm')
     const xmlStream = sax.createStream()
-    xmlStream.on('opentag', opentag)
+
+    const ctx = { nds: [] }
+    let elements = 0
+    xmlStream.on('opentag', e => {
+        opentag(ctx, e as sax.Tag)
+        elements++
+        if (elements % 1_000_000 === 0) console.debug('progress', elements)
+    })
     fileStream.pipe(xmlStream)
     await new Promise(done => xmlStream.on('end', done))
 }
 
-const result: Record<string, string[] | null> = {}
-let elements = 0
-const current = {
-    parent: undefined! as sax.Tag,
-    nds: [] as string[]
+const db = await open({ filename: `resource/data_${new Date().getTime()}.db`, driver: sqlite3.Database })
+await db.run(
+    sql`create table Node (
+        id TEXT UNIQUE NOT NULL,
+        lon TEXT NOT NULL,
+        lat TEXT NOT NULL
+    )`
+)
+await db.run(
+    sql`create table Way (
+        id TEXT UNIQUE NOT NULL,
+        highway TEXT
+    )`
+)
+await db.run(
+    sql`create table NodeWay (
+        nodeId TEXT NOT NULL,
+        wayId TEXT NOT NULL
+    )`
+)
+
+type Result = {
+    nodes: Record<string, { lon: string; lat: string } | null>
+    ways: Record<string, { highway: string; nds: string[] }>
 }
 
-await pipeline((e: sax.Tag) => {
+const result: Result = {
+    nodes: {},
+    ways: {}
+}
+
+await pipeline((ctx, e) => {
     switch (e.name) {
         case 'NODE': {
-            current.parent = e
+            ctx.parent = e
             break
         }
         case 'RELATION': {
-            current.parent = e
+            ctx.parent = e
             break
         }
         case 'WAY': {
-            current.parent = e
-            current.nds.length = 0
+            ctx.parent = e
+            ctx.nds.length = 0
             break
         }
         case 'ND': {
-            if (current.parent.name !== 'WAY') break
-            current.nds.push(e.attributes.REF as string)
+            if (ctx.parent?.name !== 'WAY') break
+            ctx.nds.push(e.attributes.REF as string)
             break
         }
         case 'TAG': {
-            if (current.parent.name !== 'WAY') break
+            if (ctx.parent?.name !== 'WAY') break
             if (e.attributes.K === 'highway') {
-                for (const nd of current.nds) {
-                    result[nd] = null
+                for (const nd of ctx.nds) {
+                    result.nodes[nd] = null
                 }
+                const id = ctx.parent.attributes.ID as string
+                result.ways[id] = { highway: e.attributes.V, nds: ctx.nds }
             }
             break
         }
     }
-    elements++
-    if (elements % 100000 === 0) console.debug('progress', elements)
 })
-console.debug('highway nodes', Object.keys(result).length)
+console.debug('highway ways', Object.keys(result.ways).length)
 
-await pipeline((e: sax.Tag) => {
+await pipeline((_ctx, e) => {
     switch (e.name) {
         case 'NODE': {
             const id = e.attributes.ID as string
-            if (result[id] === null) {
-                result[id] = [e.attributes.LON as string, e.attributes.LAT as string]
+            if (result.nodes[id] === null) {
+                result.nodes[id] = { lon: e.attributes.LON as string, lat: e.attributes.LAT as string }
             }
             break
         }
     }
-    elements++
-    if (elements % 100000 === 0) console.debug('progress', elements)
 })
+console.debug('highway nodes', Object.keys(result.nodes).length)
 
-await writeFile('resource/highway.json', JSON.stringify(result))
+console.debug('populating nodes')
+await db.run('begin')
+const nodeEntries = Object.entries(result.nodes)
+for (const [nodeId, node] of nodeEntries) {
+    if (node) {
+        await db.run(sql`insert into Node (id, lon, lat) values (?, ?, ?)`, nodeId, node.lon, node.lat)
+    }
+}
+await db.run('commit')
+
+console.debug('populating ways')
+await db.run('begin')
+const wayEntries = Object.entries(result.ways)
+for (const [wayId, way] of wayEntries) {
+    await db.run(sql`insert into Way (id, highway) values (?, ?)`, wayId, way.highway)
+}
+await db.run('commit')
+
+console.debug('populating nodeways')
+await db.run('begin')
+for (const [wayId, way] of wayEntries) {
+    for (const node of way.nds) {
+        if (node) await db.run(sql`insert into NodeWay (nodeId, wayId) values (?, ?)`, node, wayId)
+    }
+}
+await db.run('commit')
+
+console.debug('creating indexes')
+await db.run(sql`create index nodeWayNodeIdIdx on NodeWay(nodeId)`)
+await db.run(sql`create index nodeWayWayIdIdx on NodeWay(wayId)`)
