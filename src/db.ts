833c891498fa2137342ee83dcf4134159e0c00d8
ivanjermakov <ivanjermakov1@gmail.com>
2026-05-05 11:39:28 +0200

    Drop bun

--- src/db.ts
+++ src/db.ts
@@ -9,18 +9,18 @@ export let db: Database
 export const initDb = async (): Promise<Database> => {
     const db_ = await open({ filename: 'database.db', driver: sqlite3.Database })
     db = new Proxy(db_, {
-        get: (target, key) => {
-            const ret = (<any>target)[key]
+        get: (target, key: string) => {
+            const ret = (target as any)[key]
             if (typeof ret !== 'function') return ret
 
             const start = performance.now()
             return (...args: any[]) => {
                 const queryMethods = ['run', 'exec', 'all']
-                if (queryMethods.includes(<string>key)) {
+                if (queryMethods.includes(key)) {
                     debug('query', args[0], 'with args', args.slice(1))
                 }
                 const result = Reflect.apply(ret, db_, args)
-                if (queryMethods.includes(<string>key)) {
+                if (queryMethods.includes(key)) {
                     const end = performance.now()
                     debug(`query took ${(end - start).toFixed(2)}ms`)
                 }
